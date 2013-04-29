568ebc817b1c5d2e2ded2dfc22adef06b3805716
Jake Teton-Landis <just.1.jake@gmail.com>
2013-04-29 03:03:21 -0700

    seperated color space and image creation

---
+++ image/ascii/color.go
@@ -0,0 +1,56 @@
+package ascii
+
+// implements an image type and color pallete using UTF8 runes
+// because.
+
+import (
+    "image/color"
+)
+
+// UTF COLORSPACE
+// PALETTE DEFINITION
+// TODO: better palette, with more UTF codepoints
+var txtPallete = []TextColor{ ' ', '.', ':', 'o', 'O', '8', '@', '#', }
+
+// first -- colorspace: TEXT
+// mostly a mockery of color.Grey
+type TextColor rune
+func createPalleteMap (colors []TextColor) map[TextColor]uint32 {
+    value := len(colors) / 255
+    mp := make(map[TextColor]uint32, len(colors))
+
+    for i, r := range colors {
+        mp[r] = uint32(i * value)
+        mp[r] |= mp[r] << 8
+    }
+    return mp
+}
+func createPallete (colors []TextColor) color.Palette {
+    pal := make([]color.Color, len(colors))
+    for i, c := range colors {
+        pal[i] = color.Color(c)
+    }
+    return color.Palette(pal)
+}
+
+// precomute uint32 lookups for color conversion
+var txtPalleteMap = createPalleteMap(txtPallete)
+func (c TextColor) RGBA() (r, g, b, a uint32) {
+    y := txtPalleteMap[c]
+    return y, y, y, 0xffff
+}
+
+// and here's the color space!
+var Palette color.Palette = createPallete(txtPallete)
+
+
+// color model using that palette
+func textModel (c color.Color) color.Color {
+    if _, ok := c.(TextColor); ok {
+        return c
+    }
+    return Palette.Convert(c)
+}
+
+// The color model for ASCII images
+var TextModel color.Model = color.ModelFunc(textModel)
--- image/ascii/image.go
+++ image/ascii/image.go
@@ -10,52 +10,6 @@ import (
     "io" // for a silly reason ;)
 )
 
-// UTF COLORSPACE
-// PALETTE DEFINITION
-// TODO: better palette, with more UTF codepoints
-var txtPallete = []TextColor{ ' ', '.', ':', 'o', 'O', '8', '@', '#', }
-
-// first -- colorspace: TEXT
-// mostly a mockery of color.Grey
-type TextColor rune
-func createPalleteMap (colors []TextColor) map[TextColor]uint32 {
-    value := len(colors) / 255
-    mp := make(map[TextColor]uint32, len(colors))
-
-    for i, r := range colors {
-        mp[r] = uint32(i * value)
-        mp[r] |= mp[r] << 8
-    }
-    return mp
-}
-func createPallete (colors []TextColor) color.Palette {
-    pal := make([]color.Color, len(colors))
-    for i, c := range colors {
-        pal[i] = color.Color(c)
-    }
-    return color.Palette(pal)
-}
-
-// precomute uint32 lookups for color conversion
-var txtPalleteMap = createPalleteMap(txtPallete)
-func (c TextColor) RGBA() (r, g, b, a uint32) {
-    y := txtPalleteMap[c]
-    return y, y, y, 0xffff
-}
-
-// and here's the color space!
-var Palette color.Palette = createPallete(txtPallete)
-
-
-// color model using that palette
-func textModel (c color.Color) color.Color {
-    if _, ok := c.(TextColor); ok {
-        return c
-    }
-    return Palette.Convert(c)
-}
-var TextModel color.Model = color.ModelFunc(textModel)
-
 // TextImage type!
 
 // cols[rows[letters]]
